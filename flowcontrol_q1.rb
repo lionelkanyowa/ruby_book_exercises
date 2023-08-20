e586ddab2405664cbd059a25524a304b3a147d0e
Lionel Kanyowa <lionelkanyowa@gmail.com>
2023-08-20 14:19:21 -0400

    added flowcontrol chapter questions

---
+++ flowcontrol_q1.rb
@@ -0,0 +1,11 @@
+=begin
+Q.1
+Write down whether the following expressions return true or false. 
+Then type the expressions into irb to see the results.
+=end
+
+(32 * 4) >= 129 #retuns false
+false != !true #returns false
+true == 4 #returns false
+false == (847 == '847') #returns false
+(!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false #returns true
---
+++ flowcontrol_q6.rb
@@ -0,0 +1,11 @@
+=begin
+Write down whether the following expressions return true or false or raise an error. 
+Then, type the expressions into irb to see the results.
+=end
+
+(32 * 4) >= "129" #Returns false
+847 == '847' #Returns false
+'847' < '846' #Returns true
+'847' > '846' #Returns false
+'847' > '8478' #Returns false
+'847' < '8478' #Returns true
